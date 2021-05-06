a291eb930fae81f3ac3d606db34980ab230833cd
codehacker830 <senior.webinar@gmail.com>
2021-05-06 23:12:29 +0800

    :sparkles: Styling according to pathname

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -7,6 +7,10 @@ import Gro from './routes/gro';
 import Home from './routes/home';
 
 function App() {
+   const { pathname } = useLocation();
+   if (pathname === '/') require('../assets/css/et-core-unified-6-16200620328472.css');
+   else require('../assets/css/et-core-unified-6-16201824223931.css');
+   
    const [pageYOffset, setPageYOffset] = React.useState(0);
 
    React.useEffect(() => {
@@ -17,7 +21,7 @@ function App() {
       setPageYOffset(window.pageYOffset);
    }
 
-   const { pathname } = useLocation();
+
    const homeClasses = "home page-template-default page page-id-6 et_pb_button_helper_class et_fixed_nav et_show_nav et_hide_nav et_primary_nav_dropdown_animation_fade et_secondary_nav_dropdown_animation_fade et_header_style_left et_pb_footer_columns4 et_cover_background et_pb_gutter windows et_pb_gutters3 et_pb_pagebuilder_layout et_smooth_scroll et_no_sidebar et_divi_theme et-db et_minified_js et_minified_css";
    const groClasses = "page-template-default page page-id-51 et_pb_button_helper_class et_fixed_nav et_show_nav et_primary_nav_dropdown_animation_fade et_secondary_nav_dropdown_animation_fade et_header_style_left et_pb_footer_columns4 et_cover_background et_pb_gutter windows et_pb_gutters3 et_pb_pagebuilder_layout et_smooth_scroll et_no_sidebar et_divi_theme et-db et_minified_js et_minified_css chrome";
    return (
